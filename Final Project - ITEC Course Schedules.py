dd466070f39da4d792f58249957f5ad1975c5937
Johnny Ly <johnny.ly@go.minneapolis.edu>
2019-12-04 12:03:06 -0600

    Pre final project start notes

---
+++ Final Project - ITEC Course Schedules.py
@@ -0,0 +1,12 @@
+# Pre Project Notes
+
+# Write a program that reads the MCTC "Course Search Results" page, parses that page and creates a spreadsheet containing 7 columns
+# 1. ID Number (ie. 000096)
+# 2. Course Number (ie. 1150-61)
+# 3. Course Title (ie. Programming Logic and Design)
+# 4. Day of the week (ie. Wednesday)
+# 5. Time the course meet (ie. 9:00am - 11:59am)
+# 6. The credits for the course
+# 7. The instructor
+
+
