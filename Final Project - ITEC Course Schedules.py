d271bf8d8cdb239f5f9a607761f9cd431ba13ea0
Johnny Ly <johnny.ly@go.minneapolis.edu>
2019-12-15 23:45:08 -0600

    12/15 Update 11:44PM List of list of the text

--- Final Project - ITEC Course Schedules.py
+++ Final Project - ITEC Course Schedules.py
@@ -10,8 +10,9 @@ Write a program that reads the MCTC "Course Search Results" page, parses that pa
 7. The instructor'''
 
 # requests module lets you easily download files from the Web
+import requests
 # Had to install beautifulsoup4 or bs4 which allows me to extract information from an HTML page
-import requests, bs4
+from bs4 import BeautifulSoup
 
 # Request the URL we want to work with
 res = requests.get('https://eservices.minnstate.edu/registration/search/advancedSubmit.html?campusid=305&searchrcid'
@@ -27,9 +28,31 @@ except Exception as exc:
     print('There was a problem: %s' % (exc))
 
 # Creates a beautiful soup object that is stored in the variable soup
-soup = bs4.BeautifulSoup(res.content, 'html.parser')
-# Return value
-type(soup)
+soup = BeautifulSoup(res.content, 'html.parser')
 
-Container = soup.select('div', class_='yui-dt')
+# container variable that stores the 'main' div or section of the web page
+container = soup.find(id='main')
+
+#id_number = container.find_all(class_='yui-dt0-col-ID yui-dt-col-ID yui-dt-sortable')
+
+#rows = container.find_all('tr')
+
+#for tr in rows:
+   # td = tr.find_all('td')
+   # row = [i.text for i in td]
+   # print(row)
+
+
+resRows = []
+rowTags = soup.find_all('tr')
+for rowTag in rowTags:
+    columnTags = rowTag.findAll('td')
+    resCol = []
+    for columnTag in columnTags:
+        resCol.append(columnTag.text.strip())
+    resRows.append(resCol)
+print(resRows)
+
+
+
 
